45089e986ad2587e8ab60ee9423ca3b7925c805a
KoesJin <wlstjr2011@naver.com>
2024-10-10 15:24:54 +0900

    임시 시간표 페이지 작성

--- src/page/Main/SchedulePage.jsx
+++ src/page/Main/SchedulePage.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from '../../css/MainPage/SchedulePage/SchedulePage.module.css';
 
 const SchedulePage = () => {
     return (
-        <>
-            <div>임시 시간표 페이지</div>
-            <div>
-                <Link to="/mainpage">뒤로가기</Link>
+        <div className={styles.wrap}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>시간표</h1>
+                <div className={styles.table}>
+                    <div className={styles.row}>
+                        <div className={styles.headerCell}></div>
+                        <div className={styles.headerCell}>월요일</div>
+                        <div className={styles.headerCell}>화요일</div>
+                        <div className={styles.headerCell}>수요일</div>
+                        <div className={styles.headerCell}>목요일</div>
+                        <div className={styles.headerCell}>금요일</div>
+                    </div>
+
+                    {/* 각 시간별 시간표 내용 */}
+                    {[...Array(8)].map((_, i) => (
+                        <div className={styles.row} key={i}>
+                            <div className={styles.timeCell}>{i + 1}교시</div>
+                            <div className={styles.cell}>수업</div>
+                            <div className={styles.cell}>수업</div>
+                            <div className={styles.cell}>수업</div>
+                            <div className={styles.cell}>수업</div>
+                            <div className={styles.cell}>수업</div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className={styles.backButtonWrapper}>
+                    <Link to="/mainpage" className={styles.backButton}>
+                        뒤로가기
+                    </Link>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
