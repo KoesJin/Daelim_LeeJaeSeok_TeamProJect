a11994cfad1ae3ea6ffc82da62c1bf52dc4ddef0
KoesJin <wlstjr2011@naver.com>
2024-10-12 11:43:39 +0900

    Css 수정 - 스크롤 안되는 영역 수정

--- src/page/Main/ToolPage.jsx
+++ src/page/Main/ToolPage.jsx
@@ -13,61 +13,63 @@ const ToolPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>수업 도구</h1>
+        <div className={styles.ScrollContainer}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>수업 도구</h1>
 
-            <div className={styles.toolGrid}>
-                {/* 투표 도구 */}
-                <div
-                    className={styles.toolItem}
-                    // onClick={() => navigate('/vote')}
-                    onClick={() => alert('준비중입니다.')}
-                >
-                    <VoteIcon className={styles.icon} />
-                    <p className={styles.toolName}>투표</p>
+                <div className={styles.toolGrid}>
+                    {/* 투표 도구 */}
+                    <div
+                        className={styles.toolItem}
+                        // onClick={() => navigate('/vote')}
+                        onClick={() => alert('준비중입니다.')}
+                    >
+                        <VoteIcon className={styles.icon} />
+                        <p className={styles.toolName}>투표</p>
+                    </div>
+
+                    {/* 사다리타기 도구 */}
+                    <div
+                        className={styles.toolItem}
+                        // onClick={() => navigate('/ladder')}
+                        onClick={() => alert('준비중입니다.')}
+                    >
+                        <LadderIcon className={styles.icon} />
+                        <p className={styles.toolName}>사다리타기</p>
+                    </div>
+
+                    {/* 퀴즈 도구 */}
+                    <div
+                        className={styles.toolItem}
+                        // onClick={() => navigate('/quiz')}
+                        onClick={() => alert('준비중입니다.')}
+                    >
+                        <QuizIcon className={styles.icon} />
+                        <p className={styles.toolName}>퀴즈</p>
+                    </div>
+
+                    {/* 랜덤 추첨기 도구 */}
+                    <div
+                        className={styles.toolItem}
+                        // onClick={() => navigate('/random-picker')}
+                        onClick={() => alert('준비중입니다.')}
+                    >
+                        <RandomPickerIcon className={styles.icon} />
+                        <p className={styles.toolName}>랜덤 추첨기</p>
+                    </div>
+
+                    {/* 자리 선정 도구 */}
+                    <div className={styles.toolItem} onClick={() => alert('준비중입니다.')}>
+                        <SeatIcon className={styles.icon} />
+                        <p className={styles.toolName}>자리 선정</p>
+                    </div>
                 </div>
 
-                {/* 사다리타기 도구 */}
-                <div
-                    className={styles.toolItem}
-                    // onClick={() => navigate('/ladder')}
-                    onClick={() => alert('준비중입니다.')}
-                >
-                    <LadderIcon className={styles.icon} />
-                    <p className={styles.toolName}>사다리타기</p>
-                </div>
-
-                {/* 퀴즈 도구 */}
-                <div
-                    className={styles.toolItem}
-                    // onClick={() => navigate('/quiz')}
-                    onClick={() => alert('준비중입니다.')}
-                >
-                    <QuizIcon className={styles.icon} />
-                    <p className={styles.toolName}>퀴즈</p>
-                </div>
-
-                {/* 랜덤 추첨기 도구 */}
-                <div
-                    className={styles.toolItem}
-                    // onClick={() => navigate('/random-picker')}
-                    onClick={() => alert('준비중입니다.')}
-                >
-                    <RandomPickerIcon className={styles.icon} />
-                    <p className={styles.toolName}>랜덤 추첨기</p>
-                </div>
-
-                {/* 자리 선정 도구 */}
-                <div className={styles.toolItem} onClick={() => alert('준비중입니다.')}>
-                    <SeatIcon className={styles.icon} />
-                    <p className={styles.toolName}>자리 선정</p>
-                </div>
-            </div>
-
-            <div className={styles.backButtonWrapper}>
-                <Link to="/mainpage" className={styles.backButton}>
-                    뒤로가기
-                </Link>
+                {/* <div className={styles.backButtonWrapper}>
+                    <Link to="/mainpage" className={styles.backButton}>
+                        뒤로가기
+                    </Link>
+                </div> */}
             </div>
         </div>
     );
